a3f93f094667a8268f17a98a57e1cb1efc24f2fa
Theodore Chuang <chuangtheodore@gmail.com>
2018-10-22 18:43:16 +1100

    Sanitized addition User parameter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,10 @@
 class ApplicationController < ActionController::Base
-  before_action :configure_devise_params, if: :devise_controller?
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
-  def configure_devise_params
+  protected
+
+  def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :bio, :display_email])
-    devise_parameter_sanitizer.permit(:accout_update, keys: [:name, :bio, :display_email])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :bio, :display_email])
   end
 end
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -9,7 +9,7 @@ Devise.setup do |config|
   # Devise will use the `secret_key_base` as its `secret_key`
   # by default. You can change it below and use your own secret key.
   # config.secret_key = '319cf797a679560a1260f3f1601a5aa3c0f53435755e01eabb4fd97deee6fd54cb5803f70d04db834802aeaebab2e7012b0a2ae6e72cdaba7eca5b24330147e9'
-  
+
   # ==> Controller configuration
   # Configure the parent class to the devise controllers.
   # config.parent_controller = 'DeviseController'
@@ -18,7 +18,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'
+  config.mailer_sender = "onepercentbetter.365@gmail.com"
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
@@ -30,7 +30,7 @@ Devise.setup do |config|
   # Load and configure the ORM. Supports :active_record (default) and
   # :mongoid (bson_ext recommended) by default. Other ORMs may be
   # available as additional gems.
-  require 'devise/orm/active_record'
+  require "devise/orm/active_record"
 
   # ==> Configuration for any authentication mechanism
   # Configure which keys are used when authenticating a user. The default is
